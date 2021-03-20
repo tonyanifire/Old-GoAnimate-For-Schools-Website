dc939beadcf532f1ca75976ff581e7a84127eb8c
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-03-20 15:14:27 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -27,57 +27,17 @@ module.exports = function (req, res, url) {
 			title = 'Character Creator';
 			attrs = {
 				data: process.env.SWF_URL + '/cc.swf', // data: 'cc.swf',
-				type: 'application/x-shockwave-flash', 
-				id: 'char_creator', 
-				width: '960', 
-				height: '600', 
-				style:'display:block;margin-left:auto;margin-right:auto;',
+				type: 'application/x-shockwave-flash', id: 'char_creator', width: '100%', height: '100%',
 			};
 			params = {
 				flashvars: {
 					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>',
 					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'original_asset_id': query['id'] || null,
-					'themeId': 'business', 'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go',
+					'themeId': 'business', 'ut': 30, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go',
 					'm_mode': 'school', 'isLogin': 'Y', 'isEmbed': 1, 'ctc': 'go', 'tlang': 'en_US',
 				},
 				allowScriptAccess: 'always',
 				movie: process.env.SWF_URL + '/cc.swf', // 'http://localhost/cc.swf'
-			};
-			break;
-		}
-		
-		case "/cc_browser": {
-			title = "CC Browser";
-			attrs = {
-				data: process.env.SWF_URL + "/cc_browser.swf", // data: 'cc_browser.swf',
-				type: "application/x-shockwave-flash",
-				id: "char_creator",
-				width: '100%', 
-				height: '600', 
-				style:'display:block;margin-left:auto;margin-right:auto;',
-			};
-			params = {
-				flashvars: {
-					apiserver: "/",
-					storePath: process.env.STORE_URL + "/<store>",
-					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
-					original_asset_id: query["id"] || null,
-					themeId: "family",
-					ut: 60,
-					appCode: "go",
-					page: "",
-					siteId: "go",
-					m_mode: "school",
-					isLogin: "Y",
-                                        retut: 1,
-                                        goteam_draft_only: 1,
-					isEmbed: 1,
-					ctc: "go",
-					tlang: "en_US",
-					lid: 13,
-				},
-				allowScriptAccess: "always",
-				movie: process.env.SWF_URL + "/cc_browser.swf", // 'http://localhost/cc_browser.swf'
 			};
 			break;
 		}
@@ -93,9 +53,9 @@ module.exports = function (req, res, url) {
 			params = {
 				flashvars: {
 					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
-					'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
+					'ut': 30, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
 					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
-					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'nextUrl': '/pages/html/list.html',
+					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'nextUrl': '/html/list.html',
 				},
 				allowScriptAccess: 'always',
 			};
@@ -104,18 +64,17 @@ module.exports = function (req, res, url) {
 		}
 
 		case '/player': {
-			title = 'Video Player';
+			title = 'Player';
 			attrs = {
 				data: process.env.SWF_URL + '/player.swf',
 				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
 			};
 			params = {
 				flashvars: {
-					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'ut': 60,
+					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'ut': 30,
 					'autostart': 1, 'isWide': 1, 'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
 				},
 				allowScriptAccess: 'always',
-				allowFullScreen: 'true',
 			};
 			break;
 		}
@@ -125,89 +84,7 @@ module.exports = function (req, res, url) {
 	}
 	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
 	Object.assign(params.flashvars, query);
-	res.end(`
-	<head>
-		<script>
-			document.title='${title}',flashvars=${JSON.stringify(params.flashvars)}
-		</script>
-		<script>
-			if(window.location.pathname == '/player') {
-				function hideHeader() {
-					document.getElementById("header").style.display = "none";
-				}
-			} else if(window.location.pathname == '/go_full') {
-				function hideHeader() {
-					document.getElementById("header").style.display = "none";
-				}
-			}
-		</script>
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:400,700">
-                <link rel="stylesheet" href="/html/themelist/themelistfiles/common_combined.css.gz.css">
-                <link rel="stylesheet" href="/html/themelist/themelistfiles/videomaker.css.gz.css"><style>
-			body {
-				background: #eee;
-			}
-		</style>
-	</head>
-	
-<nav class="navbar site-nav" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            <a class="navbar-brand" href="http://web.archive.org/web/20180730045558/http://vyondhoster.000webhostapp.com/" title="GoAnimate for Schools">
-                <img src="/html/logo4s.png" alt="GoAnimate For Schools">
-            </a>
-        </div>
-
-        <ul class="nav site-nav-alert-nav hidden-xs">
-            <li>
-                <a href="http://web.archive.org/web/20180730045558/http://vyondhoster.000webhostapp.com/messages" title="Messages"><span class="glyphicon glyphicon-envelope"></span><span class="count"></span></a>
-            </li>
-            <li>
-                <a href="http://web.archive.org/web/20180730045558/http://vyondhoster.000webhostapp.com/notifications" title="Notifications"><span class="glyphicon glyphicon-bell"></span><span class="count"></span></a>
-            </li>
-        </ul>
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav navbar-right">
-
-                <li class="dropdown">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Your Account <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/student">Dashboard</a></li>
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/movies">Your Videos</a></li>
-                        <li class="divider"></li>
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/account">Account Settings</a></li>
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/profile/00lbzOKBnEro">Your Profile</a></li>
-                        <li class="divider"></li>
-                        <li><a class="logout-link" href="/web/20180730045558/http://vyondhoster.000webhostapp.com/logoff">Logout</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Explore <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/students">Students</a></li>
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/teachers">Teachers</a></li>
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/videos">Videos</a></li>
-                        <li class="divider"></li>
-                        <li><a href="http://web.archive.org/web/20180730045558/https://blog.goanimate4schools.com/">Educator Experiences</a></li>
-                        <li><a href="/web/20180730045558/http://vyondhoster.000webhostapp.com/public_faq">FAQ</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a class="hidden-sm hidden-md hidden-lg" href="/go_full">Make a Video</a>
-                    <span class="site-nav-btn hidden-xs"><a class="btn btn-green" href="/go_full">Make a Video</a></span>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-	
-	<body style="margin:0px" onload="hideHeader()">${toObjectString(attrs, params)
+	res.end(`<script>document.title='${title}',flashvars=${JSON.stringify(params.flashvars)}</script><body style="margin:0px">${toObjectString(attrs, params)
 		}</body>${stuff.pages[url.pathname] || ''}`);
 	return true;
 }
